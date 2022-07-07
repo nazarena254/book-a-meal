362dd816b0a644f6fd785780f8a4cf9b16cba3c0
blancc <mosesmuta22@gmail.com>
2022-07-07 10:06:59 +0300

    feat[component]: add order component

--- mealangular/src/app/app-routing.module.ts
+++ mealangular/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RegisterComponent } from './components/register/register.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { OrderComponent } from './components/order/order.component';
 
 const routes: Routes = [
   { path: 'login', component: LogInComponent },
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: 'profile', component: ProfileComponent },
   { path: 'menu', component: MenuComponent },
   { path: 'landing', component: LandingComponent },
+  { path: 'orders', component: OrderComponent },
 ];
 
 @NgModule({
