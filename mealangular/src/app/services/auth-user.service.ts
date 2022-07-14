33df45d2c4e764d22a514542d53b9d55668db490
blancc <mosesmuta22@gmail.com>
2022-07-14 12:27:29 +0300

    fix[local host]: remove double slash

--- mealangular/src/app/services/auth-user.service.ts
+++ mealangular/src/app/services/auth-user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from '../interface/user';
 
-const baseUrl = 'http://127.0.0.1:8000/api/v1/'
+const baseUrl = 'https://book-a-meal-xd.herokuapp.com/api/v1/'
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
