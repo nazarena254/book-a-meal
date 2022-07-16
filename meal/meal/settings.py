71d1d1c8fc2c0e0783d068b2e9728fb035be21c4
Charlotte Natasha <natashaonyango97@gmail.com>
2022-07-16 09:14:41 +0200

    settings changes

--- meal/meal/settings.py
+++ meal/meal/settings.py
@@ -131,7 +131,7 @@ else:
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS = ['meal--app.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['', '127.0.0.1']
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
